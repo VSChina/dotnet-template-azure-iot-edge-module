3fac6d2be007c6a5d8ff8e414bf96e7500321790
formulahendry <formulahendry@gmail.com>
2018-04-23 16:44:03 +0800

    nuget push ps1

---
+++ nuget-push.ps1
@@ -0,0 +1,6 @@
+if ($env:BUILD_SOURCEBRANCH -match "^refs/tags/v?[0-9]+\.[0-9]+\.[0-9]+$") {
+    echo "Nuget Push"
+    nuget push *.nupkg $env:api_key -Source https://www.nuget.org/api/v2/package
+} else {
+    echo "skipping Nuget Push"
+}
