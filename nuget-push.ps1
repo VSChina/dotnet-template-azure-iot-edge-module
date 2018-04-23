fe03479e3c142d550ab3b759353df92948e8f9a3
formulahendry <formulahendry@gmail.com>
2018-04-23 17:08:24 +0800

    update

--- nuget-push.ps1
+++ nuget-push.ps1
@@ -1,3 +1,4 @@
+echo $env:BUILD_SOURCEBRANCH
 if ($env:BUILD_SOURCEBRANCH -match "^refs/tags/v?[0-9]+\.[0-9]+\.[0-9]+$") {
     echo "Nuget Push"
     nuget push *.nupkg $env:api_key -Source https://www.nuget.org/api/v2/package
