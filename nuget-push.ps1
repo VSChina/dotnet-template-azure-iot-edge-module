b326826be94f57f7873724338c561bf25062098a
formulahendry <formulahendry@gmail.com>
2018-04-24 14:28:52 +0800

    Add BUILD_BINARIESDIRECTORY to PATH

--- nuget-push.ps1
+++ nuget-push.ps1
@@ -1,7 +1,8 @@
 echo $env:BUILD_SOURCEBRANCH
+echo $env:BUILD_BINARIESDIRECTORY
 if ($env:BUILD_SOURCEBRANCH -match "^refs/tags/v?[0-9]+\.[0-9]+\.[0-9]+$") {
-    echo "Nuget Push"
-    $env:$(Build.BinariesDirectory)\nuget.exe push *.nupkg $env:api_key -Source https://www.nuget.org/api/v2/package
+    $env:PATH = $env:PATH + ";$env:BUILD_BINARIESDIRECTORY"
+    nuget push *.nupkg $env:api_key -Source https://www.nuget.org/api/v2/package
 } else {
     echo "skipping Nuget Push"
 }
